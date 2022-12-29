fc84b0982f7eacbe0c735a37f1ddb85c542a7e1c
EvansRobbie <evansitoden94@gmaigit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add>
2022-12-29 11:02:47 +0300

    Complete ecommerce Drf without auth

--- api/Serializers.py
+++ api/Serializers.py
@@ -1,12 +1,125 @@
-from rest_framework.serializers import ModelSerializer
-from storeapp.models import Product, Category
+from rest_framework import serializers
+from storeapp.models import Product, Category, Review, Cart, Cartitems, ProductImage
 
-class ProductSelializer(ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id','name','description' ,'slug','inventory','old_price','price','category']
 
-class CategorySerializer(ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields= ['category_id','title', 'slug']
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'product', 'image']
+
+class ProductSerializer(serializers.ModelSerializer):
+    # add multiple images
+    images = ProductImageSerializer(many = True, read_only = True)
+    uploaded_images = serializers.ListField(
+        child = serializers.ImageField(max_length = 1000000,allow_empty_file = False,use_url = False), write_only = True)
+    
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'description' 
+            ,'slug',
+            # 'get_imageUrl',
+            'inventory','old_price',
+            'price',
+            'category',
+            'images', 
+            'uploaded_images'
+        ]
+    # Serialization Relationship
+    # Method one outputs the category name only
+     # category = serializers.StringRelatedField()
+    # Method 2 tooutput category as an object
+    category = CategorySerializer()
+    
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images')
+        product = Product.objects.create(**validated_data)
+        for image in uploaded_images:
+            new_product_image = ProductImage.objects.create(product = product, image = image)
+
+        return product
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'date_created', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id = product_id, **validated_data)
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name','price']
+
+
+class CartItemsSerializer(serializers.ModelSerializer):
+    subTotal = serializers.SerializerMethodField(method_name='Total')
+    class Meta:
+        model = Cartitems
+        fields = ['id', 'product', 'quantity','subTotal']
+    # product = serializers.StringRelatedField()
+    product  = SimpleProductSerializer(many = False)
+    def Total(self, cartitem:Cartitems):
+        total = cartitem.quantity * cartitem.product.price
+        return total
+
+class AddCartItemsSerializer(serializers.ModelSerializer):
+    product_id = serializers.UUIDField()
+    # give a friendly error if the plroduct_id doesn't exist:
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk =  value).exists():
+            raise serializers.ValidationError('There is no error with the given ID')
+
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+
+        try:
+            cartItem = Cartitems.objects.get(product_id = product_id, cart_id = cart_id)
+            cartItem.quantity += quantity
+            cartItem.save()
+
+            self.instance = cartItem
+        except:
+           self.instance = Cartitems.objects.create(cart_id = cart_id, **self.validated_data) #
+        
+        return self.instance
+
+    class Meta:
+        model = Cartitems
+        fields = ['id', 'product_id', 'quantity']
+class UpdateCartitemsSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only = True)
+    class Meta:
+        model = Cartitems
+        fields = [ 'quantity']
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+    id =  serializers.UUIDField(read_only = True)
+    grand_total = serializers.SerializerMethodField(method_name='main_total')
+    items = CartItemsSerializer(many = True, read_only = True)
+    class Meta:
+        model = Cart
+        fields = ['id','items', 'grand_total']
+
+    def main_total(self, cart: Cart):
+        items = cart.items.all()
+        total = sum([item.quantity * item.product.price for item in items])
+        return total
+
+
+
+
--- api/views.py
+++ api/views.py
@@ -2,16 +2,36 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .Serializers import ProductSelializer, CategorySerializer
-from storeapp.models import Product, Category
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .Serializers import ProductSerializer, CategorySerializer, ReviewSerializer, CartSerializer,CartItemsSerializer,AddCartItemsSerializer,UpdateCartitemsSerializer
+from storeapp.models import Product, Category, Review, Cart,Cartitems
+# Filtering
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilterSet
+# Search
+from rest_framework.filters import SearchFilter,OrderingFilter
+# Pagination
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 
-class ApiProducts(ListCreateAPIView):
+class ProductViewSets(ModelViewSet):
     queryset  = Product.objects.all()
-    serializer_class  = ProductSelializer
+    serializer_class  = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter,OrderingFilter]
+    # filterset_fields = ['category_id']
+    filterset_class = ProductFilterSet
+    # search
+    search_fields = ['name', 'description']
+    # ordering
+    ordering_fields = ['old_price']
+    # pagination
+    pagination_class = PageNumberPagination
+    """
+        Whenever we have similar classes like this, we can opt to use viewsets, Not a must but one cans use them.
+        when using viewsets, we use routers
+    """
     # def get(self, request):
     #     product = Product.objects.all()
     #     serializer = ProductSelializer(product, many = True)
@@ -23,9 +43,9 @@ class ApiProducts(ListCreateAPIView):
     #     return Response(serializer.data, status=status.HTTP_200_OK)
     #     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-class ApiProduct(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSelializer
+# class ApiProduct(RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSelializer
 
     # def get(self, request, pk):
     #     instance = get_object_or_404(Product, id = pk)
@@ -46,7 +66,7 @@ class ApiProduct(RetrieveUpdateDestroyAPIView):
 
 
 
-class ApiCategories(ListCreateAPIView):
+class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # def get(self, request):
@@ -60,9 +80,9 @@ class ApiCategories(ListCreateAPIView):
     #     serializer.save()
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class ApiCategory(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+# class ApiCategory(RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
     # def get(self, request,pk):
     #     category = get_object_or_404(Category, category_id = pk)
     #     serializer = CategorySerializer(category)
@@ -79,6 +99,43 @@ class ApiCategory(RetrieveUpdateDestroyAPIView):
     #     category = get_object_or_404(Category, category_id = pk)
     #     category.delete()
     #     return Response({'res': 'Category deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+class ReviewViewSet(ModelViewSet):
+    # queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id = self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin,DestroyModelMixin, GenericViewSet):
+    queryset =  Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    # to avoid conflicts between the put and the patch you cal list out the required trrps methods
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    # queryset = Cartitems.objects.all()---gets all items of every cart in general
+    # Use querry set function to to get item of a specic cart - fetch item for only a given cart
+    def get_queryset(self):
+        return Cartitems.objects.filter(cart_id = self.kwargs['cart_pk'])
+    # serializer_class = CartItemsSerializer
+    # get self to add a product to the item
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemsSerializer
+        
+        # to update a particular field in the model we use PATCH
+        elif self.request.method == 'PATCH':
+            return UpdateCartitemsSerializer
+
+        return  CartItemsSerializer
+
+    # use get_serializer_context to get the specific cart id
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
 
 
 
